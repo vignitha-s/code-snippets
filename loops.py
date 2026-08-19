9d1f769a66b8002ecda5c4903255e3a9fa578adb
vignitha-s <nvignitha_cse2405h0@mgit.ac.in>
2026-08-19 14:47:30 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,18 @@
+# Python Loop Snippets
+
+# For loop
+for i in range(1, 6):
+    print(i)
+
+# Loop through a list
+fruits = ["apple", "banana", "orange"]
+
+for fruit in fruits:
+    print(fruit)
+
+# While loop
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
